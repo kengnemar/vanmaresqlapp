7af85c82390ad4ad3d3a5aeed62995b2f10e2aa5
admin <admin@DESKTOP-RI0NAAP>
2023-08-19 16:23:51 -0700

    Add project files.

---
+++ vanmaresqlapp/Pages/Index.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using vanmaresqlapp.Models;
+using vanmaresqlapp.Services;
+
+namespace vanmaresqlapp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public List<Product> Products;
+        public void OnGet()
+        {
+            ProductService productService = new ProductService();
+            Products = productService.GetProducts();
+        }
+
+    }
+
+}
